55ab22219c8a4a0e9c79bbba279c97c33874b440
VSoups <vince.kubota@gmail.com>
2024-07-24 15:16:52 -0700

    Return json to front end template fnc

--- controllers/api/cards.js
+++ controllers/api/cards.js
@@ -5,8 +5,26 @@ module.exports = {
 };
 
 async function getCardByName(req, res) {
-    console.log(`Ctrl card name: ${req.params.cardName}`);
+    // fetch card json from Scryfall
     const fetchCard = await fetch(`https://api.scryfall.com/cards/named?fuzzy=${req.params.cardName}`)
-    .then(res => res.json()).then(card => card);
-    console.log(fetchCard);
+    .then(res => res.json()).then(card => card); 
+    console.log(`Card id ---- ${fetchCard.id} ----`);
+
+    // Search DB for preexisting card object
+
+    // // Return data to front end (if/else)
+    // if (!card && !!fetchCard.id) {
+    //     const newCard = await Card.create(fetchCard);
+    //     console.log('Card created: ', newCard.name);
+    //     res.json(newCard);
+    // // Find card in db if it does exist
+    // } else if (card)  {
+    //     // const newCard = await Card.findOne({id: fetchCard.id});
+    //     console.log('Card found: ', card.name)
+    //     res.json(card);
+    // // User input invalid
+    // } else {
+    //     console.log('Card not found');
+    //     res.json('Card not found')
+    // }
 }
